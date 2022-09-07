527594a2ccbe358b2a4b32ff8d9ce8cfe1ecdf09
arturlector <art.igberdin@gmail.com>
2022-09-07 12:36:43 +0300

    build(app): create project

---
+++ project-base-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project-base-ios
+//
+//  Created by Artur Igberdin on 07.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
